c7b84043de1942addc8c25bd8b95aa4d6b05f540
faithremo <faithmakumi@gmail.com>
2022-02-20 16:38:21 +0300

    add a user class and a method defining properties of a user

--- user.py
+++ user.py
@@ -1,2 +1,16 @@
 import string
 import random
+
+class user:
+    """creating a user class to allow genarartion of new instances of a user.
+    """
+    user_list = []
+     
+    def __init__(self,username,password):
+         
+         """this method outlines the properties of a user.
+         """
+         self.username = username
+         self.password = password
+    
+    
