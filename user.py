c8252299577b733b0f272ca28033ed7a9adff1fe
faithremo <faithmakumi@gmail.com>
2022-02-20 16:57:20 +0300

    added a method to save a new user and another method to delete an already saved user from the user list.

--- user.py
+++ user.py
@@ -4,7 +4,7 @@ import random
 class user:
     """creating a user class to allow genarartion of new instances of a user.
     """
-    user_list = []
+    userList = []
      
     def __init__(self,username,password):
          
@@ -12,5 +12,22 @@ class user:
          """
          self.username = username
          self.password = password
+         
+         
+    def saveUser(self):
+        """this method saves a new user intance in to the user list.
+        """
+        user.userList.append(self)
+        
+    @classmethod
+    def displayUser(cls):
+        return cls.userList
+    
+    def deleteUser(self):
+        """This method deleted a saved account from the list
+        """
+        user.userList.remove(self)
+        
+    
     
     
